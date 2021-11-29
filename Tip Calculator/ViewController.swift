ee3199494b0e9895429c7548e01c4b1058908cda
danieltruong <aduy1122@gmail.com>
2021-11-28 22:01:34 -0800

    first commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var total : Double = Double()
+    @IBOutlet weak var currencyControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var billAmountTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func calculateTip(_ sender: Any) {
+        //Get bill amount from text input
+        let bill = Double(billAmountTextField.text!) ?? 0
+        let tipPercentages = [0.15,0.18,0.2]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+         total = bill + tip
+        //Update tip amount label
+        tipAmountLabel.text = String(format: "$%.2f", tip)
+        //Update Total Amount
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func convertCurrency(_ sender: Any) {
+        
+        let currencyConver = [1,23000,1194,1.28,6.38]
+        let currencySign = ["$","VND","W","CAD","NDT"]
+        let result = total * currencyConver[currencyControl.selectedSegmentIndex]
+        let currency = currencySign[currencyControl.selectedSegmentIndex]
+        totalLabel.text = String(format: "\(currency) %.2f", result)    }
+    
 }
 
